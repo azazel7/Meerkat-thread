bba3e9998650fb1a6cddb7e024bfd5404bade463
magoa <mel@statler.world>
2015-03-31 16:26:17 +0200

    Add some exemple

---
+++ exemple/main.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "meerkat.h"
+
+static void* func(int *nbb)
+{
+	int nb = *nbb;
+	int i = 1;
+	printf("func%d: started\n", nb);
+	while(i < 7)
+	{
+		printf("while %d\n", nb);
+		thread_yield();
+		/*sleep(1);*/
+		++i;
+	}
+	printf("func%d: returning\n", nb);
+	*nbb *= 3;
+	return (void*)nbb;
+}
+
+int main(int argc, char *argv[])
+{
+	int i = 0, *nb;
+	thread_t thre[5];
+	for(i = 0; i < 5; i++)
+	{
+		nb = (int *)malloc(sizeof(int));
+		*nb = i;
+		if(thread_create(&thre[i], (void* (*)(void*))func, nb) < 0)
+			break;
+	}
+	 /*nb = (int*)malloc(sizeof(int));*/
+	/**nb = i++;*/
+	 /*func(nb);*/
+	int* value;
+	for(i = 0; i < 5; ++i)
+	{
+		thread_join(thre[i], (void**)&value);
+		printf("Result for %d : %d\n", i, *value);
+		free(value);
+	}
+	fprintf(stdout, "Main ending\n");
+	thread_exit(NULL);
+	return 0;
+	/*
+	 * exit(EXIT_SUCCESS);
+	 */
+
+	/*
+	 * Install timer_handler as the signal handler for SIGVTALRM. 
+	 */
+	/*
+	 * signal(SIGVTALRM, timer_handler);
+	 */
+
+	/*
+	 * Configure the timer to expire after 250 msec... 
+	 */
+	/*
+	 * timeslice.it_value.tv_sec = 2;
+	 */
+	/*
+	 * timeslice.it_value.tv_usec = 0;
+	 */
+	/*
+	 * timeslice.it_interval = timeslice.it_value;
+	 */
+	/*
+	 * Start a virtual timer. It counts down whenever this process is
+	 * *    executing. 
+	 */
+	/*
+	 * setitimer (ITIMER_VIRTUAL, &timeslice, NULL);
+	 */
+
+	/*
+	 * Do busy work. 
+	 */
+	/*
+	 * while (1);
+	 */
+}

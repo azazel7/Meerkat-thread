18b8491be37dc3ed4748c45fc77d9d2c757acf8f
magoa <mel@statler.world>
2015-04-16 11:22:21 +0200

    Fix some warning

--- exemple/main.c
+++ exemple/main.c
@@ -16,7 +16,7 @@ static void* func(int *nbb)
 		/*sleep(1);*/
 		++i;
 	}
-	printf("func%d: returning (%p)\n", nb, thread_self());
+	printf("func%d: returning (%d)\n", nb, thread_self());
 	*nbb *= 3;
 	return (void*)nbb;
 }
@@ -33,9 +33,6 @@ int main(int argc, char *argv[])
 			break;
 		thread_yield();
 	}
-	 /*nb = (int*)malloc(sizeof(int));*/
-	/**nb = i++;*/
-	 /*func(nb);*/
 	int* value;
 	for(i = 0; i < SIZE; ++i)
 	{
@@ -46,41 +43,4 @@ int main(int argc, char *argv[])
 	}
 	fprintf(stdout, "Main ending\n");
 	thread_exit(NULL);
-	/*
-	 * exit(EXIT_SUCCESS);
-	 */
-
-	/*
-	 * Install timer_handler as the signal handler for SIGVTALRM. 
-	 */
-	/*
-	 * signal(SIGVTALRM, timer_handler);
-	 */
-
-	/*
-	 * Configure the timer to expire after 250 msec... 
-	 */
-	/*
-	 * timeslice.it_value.tv_sec = 2;
-	 */
-	/*
-	 * timeslice.it_value.tv_usec = 0;
-	 */
-	/*
-	 * timeslice.it_interval = timeslice.it_value;
-	 */
-	/*
-	 * Start a virtual timer. It counts down whenever this process is
-	 * *    executing. 
-	 */
-	/*
-	 * setitimer (ITIMER_VIRTUAL, &timeslice, NULL);
-	 */
-
-	/*
-	 * Do busy work. 
-	 */
-	/*
-	 * while (1);
-	 */
 }

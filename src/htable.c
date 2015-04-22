0016439c311744286b8ed1ac14fa6599a90d0f06
magoa <mel@statler.world>
2015-04-22 13:09:52 +0200

    Fix a double ++ in size of htable

--- src/htable.c
+++ src/htable.c
@@ -6,220 +6,219 @@
 
 typedef struct node
 {
-  void* data;
-  void* key;
+	void *data;
+	void *key;
 } htable_node;
 
 struct htable
 {
-  List *table[SIZE_HASH_TABLE];
-  mutex_t locks[SIZE_HASH_TABLE];
-  int (*hash) ();
-  bool(*cmp) ();
-  bool use_lock;
-  int size;
+	List *table[SIZE_HASH_TABLE];
+	mutex_t locks[SIZE_HASH_TABLE];
+	int (*hash) ();
+	  bool(*cmp) ();
+	bool use_lock;
+	int size;
 };
 typedef struct htable htable;
 
-static int htable__hash_int(void*);
-static bool htable__cmp_int(void*, void*);
+static int htable__hash_int(void *);
+static bool htable__cmp_int(void *, void *);
 
-htable* htable__create_int(bool use_lock)
+htable *htable__create_int(bool use_lock)
 {
-  return htable__create(htable__hash_int, htable__cmp_int, use_lock);
+	return htable__create(htable__hash_int, htable__cmp_int, use_lock);
 }
+
 htable *htable__create(int (*hash) (), bool(*cmp) (), bool use_lock)
 {
-  htable *h_table = malloc(sizeof(htable));
-  if(h_table == NULL)
-    {
-      fprintf(stderr, "Memory out of stock\n");
-      exit(EXIT_FAILURE);
-    }
-  h_table->cmp = cmp;
-  h_table->hash = hash;
-  h_table->size = 0;
-  h_table->use_lock = use_lock;
-  int i;
-  for(i = 0; i < SIZE_HASH_TABLE; i++)
-    {
-      h_table->table[i] = list__create();
-      if(h_table->table[i] == NULL)
+	htable *h_table = malloc(sizeof(htable));
+	if(h_table == NULL)
 	{
-	  fprintf(stderr, "Memory out of stock\n");
-	  exit(EXIT_FAILURE);
+		fprintf(stderr, "Memory out of stock\n");
+		exit(EXIT_FAILURE);
 	}
-    }
-  if(use_lock)
-    for(i = 0; i < SIZE_HASH_TABLE; i++)
-      if(!mutex_init(&h_table->locks[i]))
+	h_table->cmp = cmp;
+	h_table->hash = hash;
+	h_table->size = 0;
+	h_table->use_lock = use_lock;
+	int i;
+	for(i = 0; i < SIZE_HASH_TABLE; i++)
 	{
-	  perror("htable_create -> mutex_init");
-	  exit(EXIT_FAILURE);
+		h_table->table[i] = list__create();
+		if(h_table->table[i] == NULL)
+		{
+			fprintf(stderr, "Memory out of stock\n");
+			exit(EXIT_FAILURE);
+		}
 	}
-		
-  return h_table;
+	if(use_lock)
+		for(i = 0; i < SIZE_HASH_TABLE; i++)
+			if(!mutex_init(&h_table->locks[i]))
+			{
+				perror("htable_create -> mutex_init");
+				exit(EXIT_FAILURE);
+			}
+
+	return h_table;
 }
 
 bool htable__contain(htable * h_table, void *key)
 {
-  int idx = h_table->hash(key);
-  htable_node* node = NULL;
-  if(h_table->use_lock)
-    mutex_lock(&(h_table->locks[idx]));
-  list__for_each(h_table->table[idx], node)
-    {
-      if(h_table->cmp(node->key, key))
+	int idx = h_table->hash(key);
+	htable_node *node = NULL;
+	if(h_table->use_lock)
+		mutex_lock(&(h_table->locks[idx]));
+	list__for_each(h_table->table[idx], node)
 	{
-	  if(h_table->use_lock)
-	    mutex_unlock(&(h_table->locks[idx]));
-	  return true;
+		if(h_table->cmp(node->key, key))
+		{
+			if(h_table->use_lock)
+				mutex_unlock(&(h_table->locks[idx]));
+			return true;
+		}
 	}
-    }
-  if(h_table->use_lock)
-    mutex_unlock(&h_table->locks[idx]);
-return false;
+	if(h_table->use_lock)
+		mutex_unlock(&h_table->locks[idx]);
+	return false;
 }
 
-void htable__insert(htable *h_table, void* key, void *data)
+void htable__insert(htable * h_table, void *key, void *data)
 {
-  int idx = h_table->hash(key);
-  htable_node* node = malloc(sizeof(htable_node));
-  if(node == NULL)
-    {
-      fprintf(stderr, "Memory out of stock\n");
-      exit(EXIT_FAILURE);
-    }
-  node->key = key;
-  node->data = data;
-  if(h_table->use_lock)
-    mutex_lock(&h_table->locks[idx]);
-list__add_front(h_table->table[idx], node);
-if(h_table->use_lock)
-  mutex_unlock(&(h_table->locks[idx]));
-h_table->size++;
-if(h_table->use_lock)
-  __sync_add_and_fetch(&h_table->size, 1);
- else
-   h_table->size++;
-}
-
-void htable__remove(htable *h_table, void *key)
-{
-  int idx = h_table->hash(key);
-  List *list = h_table->table[idx];
-  htable_node* node = NULL;
-  if(h_table->use_lock)
-    mutex_lock(&(h_table->locks[idx]));
-  list__for_each(list, node)
-    {
-      if(h_table->cmp(node->key, key))
+	int idx = h_table->hash(key);
+	htable_node *node = malloc(sizeof(htable_node));
+	if(node == NULL)
 	{
-	  list__remove(list);
-	  free(node);
-	  if(h_table->use_lock)
-	    __sync_sub_and_fetch(&h_table->size, 1);
-	  else
-	    --h_table->size;
-	  break;
+		fprintf(stderr, "Memory out of stock\n");
+		exit(EXIT_FAILURE);
 	}
-    }
-  if(h_table->use_lock)
-    mutex_unlock(&(h_table->locks[idx]));
+	node->key = key;
+	node->data = data;
+	if(h_table->use_lock)
+		mutex_lock(&h_table->locks[idx]);
+	list__add_front(h_table->table[idx], node);
+	if(h_table->use_lock)
+		mutex_unlock(&(h_table->locks[idx]));
+	if(h_table->use_lock)
+		__sync_add_and_fetch(&h_table->size, 1);
+	else
+		h_table->size++;
 }
 
-int htable__size(htable *h_table)
+void htable__remove(htable * h_table, void *key)
 {
-  return h_table->size;
-}
-
-bool htable__get_element(htable* h_table, void ** key, void** data)
-{
-  if(h_table->size == 0)
-    return NULL;
-  int i;
-  for(i = 0; i < SIZE_HASH_TABLE; i++)
-    {
-      if(h_table->use_lock)
-	mutex_lock(&(h_table->locks[i]));
-      if(list__get_size(h_table->table[i]) > 0)
+	int idx = h_table->hash(key);
+	List *list = h_table->table[idx];
+	htable_node *node = NULL;
+	if(h_table->use_lock)
+		mutex_lock(&(h_table->locks[idx]));
+	list__for_each(list, node)
 	{
-	  htable_node* node = list__top(h_table->table[i]);
-	  if(key != NULL)
-	    *key = node->key;
-	  if(data != NULL)
-	    *data = node->data;
-	  if(h_table->use_lock)
-	    mutex_unlock(&(h_table->locks[i]));
-	  return true;
+		if(h_table->cmp(node->key, key))
+		{
+			list__remove(list);
+			free(node);
+			if(h_table->use_lock)
+				__sync_sub_and_fetch(&h_table->size, 1);
+			else
+				--h_table->size;
+			break;
+		}
 	}
-      if(h_table->use_lock)
-	mutex_unlock(&(h_table->locks[i]));
-    }
-  return false;
+	if(h_table->use_lock)
+		mutex_unlock(&(h_table->locks[idx]));
 }
 
-void htable__apply(htable *h_table, void (*function) (void *))
+int htable__size(htable * h_table)
 {
-  int i;
-  for(i = 0; i < SIZE_HASH_TABLE; i++)
-    {
-      htable_node* node = NULL;
-      if(h_table->use_lock)
-	mutex_lock(&(h_table->locks[i]));
-      list__for_each(h_table->table[i], node)
-	function(node->data);
-      if(h_table->use_lock)
-	mutex_unlock(&(h_table->locks[i]));
-    }
+	return h_table->size;
 }
 
-void *htable__find(htable *h_table, void *key)
+bool htable__get_element(htable * h_table, void **key, void **data)
 {
-  int idx = h_table->hash(key);
-  htable_node* node = NULL;
-  if(h_table->use_lock)
-    mutex_lock(&(h_table->locks[idx]));
-		
-  list__for_each(h_table->table[idx], node)
-    {
-      if(h_table->cmp(node->key, key))
+	if(h_table->size == 0)
+		return NULL;
+	int i;
+	for(i = 0; i < SIZE_HASH_TABLE; i++)
 	{
-	  if(h_table->use_lock)
-	    mutex_unlock(&(h_table->locks[idx]));
-	  return node->data;
+		if(h_table->use_lock)
+			mutex_lock(&(h_table->locks[i]));
+		if(list__get_size(h_table->table[i]) > 0)
+		{
+			htable_node *node = list__top(h_table->table[i]);
+			if(key != NULL)
+				*key = node->key;
+			if(data != NULL)
+				*data = node->data;
+			if(h_table->use_lock)
+				mutex_unlock(&(h_table->locks[i]));
+			return true;
+		}
+		if(h_table->use_lock)
+			mutex_unlock(&(h_table->locks[i]));
 	}
-    }
-  if(h_table->use_lock)
-    mutex_unlock(&(h_table->locks[idx]));
-  return NULL;
+	return false;
 }
 
-void htable__destroy(htable *h_table)
+void htable__apply(htable * h_table, void (*function) (void *))
 {
-  int i;
-  htable_node* node = NULL;
-  for(i = 0; i < SIZE_HASH_TABLE; i++)
-    {
-      if(h_table->use_lock)
-	mutex_lock(&(h_table->locks[i]));
-      list__for_each(h_table->table[i], node)
-	free(node);
-      list__destroy(h_table->table[i]);
-      if(h_table->use_lock)
-	mutex_unlock(&(h_table->locks[i]));
-    }
-  if(h_table->use_lock)
-    for(i = 0; i < SIZE_HASH_TABLE; i++)
-      mutex_destroy(&h_table->locks[i]);
-  free(h_table);
+	int i;
+	for(i = 0; i < SIZE_HASH_TABLE; i++)
+	{
+		htable_node *node = NULL;
+		if(h_table->use_lock)
+			mutex_lock(&(h_table->locks[i]));
+		list__for_each(h_table->table[i], node) function(node->data);
+		if(h_table->use_lock)
+			mutex_unlock(&(h_table->locks[i]));
+	}
 }
 
-int htable__hash_int(void* data)
+void *htable__find(htable * h_table, void *key)
 {
-  return ((long int) data) % SIZE_HASH_TABLE;
+	int idx = h_table->hash(key);
+	htable_node *node = NULL;
+	if(h_table->use_lock)
+		mutex_lock(&(h_table->locks[idx]));
+
+	list__for_each(h_table->table[idx], node)
+	{
+		if(h_table->cmp(node->key, key))
+		{
+			if(h_table->use_lock)
+				mutex_unlock(&(h_table->locks[idx]));
+			return node->data;
+		}
+	}
+	if(h_table->use_lock)
+		mutex_unlock(&(h_table->locks[idx]));
+	return NULL;
 }
-bool htable__cmp_int(void* d1, void* d2)
+
+void htable__destroy(htable * h_table)
 {
-  return (long int)d1 == (long int) d2;
+	int i;
+	htable_node *node = NULL;
+	for(i = 0; i < SIZE_HASH_TABLE; i++)
+	{
+		if(h_table->use_lock)
+			mutex_lock(&(h_table->locks[i]));
+		list__for_each(h_table->table[i], node) free(node);
+		list__destroy(h_table->table[i]);
+		if(h_table->use_lock)
+			mutex_unlock(&(h_table->locks[i]));
+	}
+	if(h_table->use_lock)
+		for(i = 0; i < SIZE_HASH_TABLE; i++)
+			mutex_destroy(&h_table->locks[i]);
+	free(h_table);
+}
+
+int htable__hash_int(void *data)
+{
+	return ((long int)data) % SIZE_HASH_TABLE;
+}
+
+bool htable__cmp_int(void *d1, void *d2)
+{
+	return (long int)d1 == (long int)d2;
 }
